e06d9197101ffeb921f7af9b3d5d96bff0145672
Lu@dfw <luwang827@gmail.com>
2021-03-22 23:14:54 -0500

    03222021Update
    IFrame added to aws blog page.

---
+++ personal-page/js/awsblog.js
@@ -0,0 +1,5 @@
+var url="https://aws.amazon.com/new/?whats-new-content-all.sort-by=item.additionalFields.postDateTime&whats-new-content-all.sort-order=desc"
+function showawsupdates(){
+	ifAwswhatsnew=document.getElementById("awswhatsnew")
+	ifAwswhatsnew.src=url;
+}
